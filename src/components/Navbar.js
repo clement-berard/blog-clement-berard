725dc6297fb41c037703350c4260e6a6bfece6d7
Clément BERARD <38923547+clement-berard@users.noreply.github.com>
2019-08-16 15:12:41 +0200

    feat(nav): responsive nav (#7)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,117 @@
 import React from 'react';
+import styled, { css } from 'styled-components';
 import { Link } from 'gatsby';
 import { FaChild, FaGithub } from 'react-icons/fa';
+import {
+  DARK_VIOLET,
+  LIGHT_VIOLET,
+  PALETTE_6,
+  LIGHT_GRAY,
+} from '../../config/theme.config';
+import { centerFlex } from '../utils/styled.utils';
 import logo from '../img/logo-cblog.svg';
 
+const NavBlock = styled.nav`
+  height: 80px;
+  border-bottom: 1px solid ${LIGHT_VIOLET};
+`;
+const NavContainerCss = css`
+  height: 100%;
+  width: 100%;
+  background-color: ${DARK_VIOLET};
+  display: grid;
+
+  & > * {
+    a {
+      transition: background-color 0.2s linear;
+      width: 100%;
+      height: 100%;
+      color: ${LIGHT_GRAY};
+      ${centerFlex}
+      &:hover {
+        color: ${LIGHT_GRAY};
+        background-color: ${LIGHT_VIOLET} !important;
+      }
+      &.site-title {
+        &:hover {
+          background-color: ${PALETTE_6} !important;
+        }
+      }
+    }
+    &:not(:last-child) {
+      border-right: 1px solid ${LIGHT_VIOLET};
+    }
+    ${centerFlex}
+  }
+
+  .site-title {
+    img {
+      height: 70px;
+    }
+  }
+`;
+
+const NavContainerDesktop = styled.div`
+  display: none;
+  @media (min-width: 769px) {
+    ${NavContainerCss};
+    grid-template-columns: 1.5fr 1fr 1fr 4fr 0.3fr 0.3fr;
+  }
+`;
+
+const NavContainerMobile = styled.div`
+  display: none;
+  @media (max-width: 768px) {
+    ${NavContainerCss};
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+`;
+
 const Navbar = () => (
-  <nav
-    className="navbar is-transparent main-nav-bar"
-    role="navigation"
-    aria-label="main-navigation"
-    id="mainNav"
-  >
-    <div className="main-nav-bar--container">
-      <div className="main-nav-bar--container--brand">
+  <NavBlock role="navigation" aria-label="main-navigation">
+    <NavContainerDesktop>
+      <div>
         <Link to="/" className="site-title">
           <img src={logo} alt="CBlog" />
         </Link>
       </div>
-      <div className="main-nav-bar--container--item">
+      <div>
         <Link to="/categories">Catégories</Link>
       </div>
-      <div className="main-nav-bar--container--item">
+      <div>
         <Link to="/tags">Tags</Link>
       </div>
       <div />
-      <div className="main-nav-bar--container--item">
+      <div>
         <a href="https://berard.dev" target="_blank" rel="noopener noreferrer">
           <FaChild />
         </a>
       </div>
-      <div className="main-nav-bar--container--item">
-        <a href="https://github.com/clement-berard" target="_blank" rel="noopener noreferrer">
+      <div>
+        <a
+          href="https://github.com/clement-berard"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <FaGithub />
         </a>
       </div>
-    </div>
-  </nav>
+    </NavContainerDesktop>
+    <NavContainerMobile>
+      <div>
+        <Link to="/" className="site-title">
+          <img src={logo} alt="CBlog" />
+        </Link>
+      </div>
+      <div>
+        <Link to="/categories">Catégories</Link>
+      </div>
+      <div>
+        <Link to="/tags">Tags</Link>
+      </div>
+      <div />
+    </NavContainerMobile>
+  </NavBlock>
 );
 
 export default Navbar;
---
+++ src/utils/styled.utils.js
@@ -0,0 +1,7 @@
+import { css } from 'styled-components';
+
+export const centerFlex = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
